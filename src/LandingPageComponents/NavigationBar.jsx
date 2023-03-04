7599c76d80b29b0c9d34944ed9d04479d4fc7eff
YiKaiLiao <yikailia@usc.edu>
2023-03-04 23:36:56 +0800

    [wip] Created FlyoutSignin.jsx

--- src/LandingPageComponents/NavigationBar.jsx
+++ src/LandingPageComponents/NavigationBar.jsx
@@ -1,12 +1,19 @@
 import './NavigationBar.scss';
 import SearchBar from '../SearchBarComponents/SearchBar';
+import FlyoutSigin from './FlyoutSignin';
+
 const NavigationBar = () => {
     return (
         <div className="navBar">
-            <div className='navLogo'> Logo </div>
-            <div className='navSearchBar'><SearchBar /></div>
-            <div className='navSignIn'>Signin</div>
-            <div className='navCart'>Cart Icon</div>
+            <div className='navBelt'>
+                <div className='navLogo'> Logo </div>
+                <div className='navSearchBar'><SearchBar /></div>
+                <div className='navSignIn'>Signin</div>
+                <div className='navCart'>Cart Icon</div>
+            </div>
+            <div className='navFlyout'>
+                <FlyoutSigin />
+            </div>
         </div>
     );
 }
